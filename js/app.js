c91cee626e3d38926ddd8967384e1fa10fd0a2fb
Wojciech Górecki <wkgorecki@gmail.com>
2018-06-13 15:20:36 +0200

    prawie skończone

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {});
+$( () => {
+
+   liDelete();
+    h2Clean();
+    pClean();
+
+    let $addBtn = $(".addNote");
+    let $ul = $("ul");
+    let $li = $("li");
+    let $killEmAll = $(".deleteAll");
+
+   // console.log($killEmAll);
+
+    $addBtn.on("click", ()=>{
+        $ul.append(`<li>     
+                <button class="delete"><img src="images/iconmonstr-x-mark-4-32.png" alt=""></button>
+                       <h2 contenteditable="true">Kliknij, by dodać nagłówek</h2>
+                <p contenteditable="true">Kliknij, by dodać tekst</p>
+                       </li>`);
+        liDelete();
+        h2Clean();
+        pClean();
+    });
+
+    function liDelete(){
+        $('.delete').click(function() {
+            $(this).parent($li).remove();
+        });
+    }
+
+    function h2Clean(){
+        $('h2').click(function() {
+            $(this).empty();
+        });
+    }
+
+    function pClean(){
+        $('p').click(function() {
+            $(this).empty();
+        });
+    }
+
+    $killEmAll.on("click", ()=> {
+        $ul.children().remove();
+
+    })
+
+});
